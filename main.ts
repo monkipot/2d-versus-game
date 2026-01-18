8ad93d061413a25bd9107c90d2a27d4721713eda
monki <trad@trackstone.fr>
2026-01-18 18:25:24 +0100

    refactor(webgl): refactor using class syntax

--- main.ts
+++ main.ts
@@ -1,19 +1,30 @@
 /**
  * https://developer.mozilla.org/fr/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL
  */
-main();
+class Game {
+    canvas: HTMLCanvasElement;
+    gl: WebGLRenderingContext;
 
-function main() {
-    const canvas = document.querySelector<HTMLCanvasElement>("#glCanvas");
-    if (!canvas) {
-        throw new Error("Cant retrieve canva element");
+    constructor() {
+        const canvas = document.querySelector<HTMLCanvasElement>("#glCanvas");
+        if (!canvas) {
+            throw new Error("Cant retrieve canva element");
+        }
+        const gl = canvas.getContext("webgl");
+
+        if (!gl) {
+            throw new Error("WebGL isnt supported");
+        }
+
+        this.canvas = canvas;
+        this.gl = gl;
     }
-    const gl = canvas.getContext("webgl");
 
-    if (!gl) {
-        throw new Error("WebGL isnt supported");
+    render(): void {
+        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
     }
-
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 }
+
+const game = new Game();
+game.render();
